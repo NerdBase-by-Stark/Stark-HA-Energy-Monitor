eb6af366c78cb8b92fb6d75a1128ad17db0b46e7
NerdBase-by-Stark <github@nerdbase.me>
2024-09-21 15:53:42 +0100

    Update const.py

--- custom_components/stark_energy_monitor/const.py
+++ custom_components/stark_energy_monitor/const.py
@@ -1,3 +1,10 @@
 """Constants for the Stark Energy Monitor integration."""
 
 DOMAIN = "stark_energy_monitor"
+
+PLATFORMS = ["sensor", "binary_sensor", "switch", "notify"]
+
+CONF_MONITOR_NAME = "monitor_name"
+CONF_SAMPLE_INTERVAL = "sample_interval"
+CONF_ENABLE_NOTIFICATIONS = "enable_notifications"
+CONF_DATA_RETENTION_DAYS = "data_retention_days"
