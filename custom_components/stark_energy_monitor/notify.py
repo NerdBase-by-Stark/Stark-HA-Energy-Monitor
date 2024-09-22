e594f2d47c17a8b63c7486b4e8d16f8f2a1f0400
NerdBase-by-Stark <github@nerdbase.me>
2024-09-22 22:47:04 +0100

    Notify update

--- custom_components/stark_energy_monitor/notify.py
+++ custom_components/stark_energy_monitor/notify.py
@@ -1,6 +1,6 @@
 """Notifications for Stark Energy Monitor."""
 import logging
-from homeassistant.components.notify import BaseNotificationService
+from homeassistant.components.notify import NotifyEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -15,13 +15,15 @@ async def async_setup_entry(
     """Set up the Stark Energy Monitor notification service."""
     async_add_entities([StarkEnergyMonitorNotificationService(hass, entry)])
 
-class StarkEnergyMonitorNotificationService(BaseNotificationService):
-    """Notification service for Stark Energy Monitor."""
+class StarkEnergyMonitorNotificationService(NotifyEntity):
+    """Notification entity for Stark Energy Monitor."""
 
     def __init__(self, hass: HomeAssistant, entry: ConfigEntry):
-        """Initialize the notification service."""
+        """Initialize the notification entity."""
         self.hass = hass
         self.entry = entry
+        self._attr_name = "Stark Energy Monitor Notification"
+        self._attr_unique_id = "stark_energy_monitor_notification"
 
     async def async_send_message(self, message="", **kwargs):
         """Send a notification message asynchronously."""
