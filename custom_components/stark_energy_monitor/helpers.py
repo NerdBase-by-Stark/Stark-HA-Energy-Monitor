c0a170b37017fc943ae6c2f5293b34ffef020b3a
NerdBase-by-Stark <github@nerdbase.me>
2024-09-24 21:20:17 +0100

    Remove Notify

--- custom_components/stark_energy_monitor/helpers.py
+++ custom_components/stark_energy_monitor/helpers.py
@@ -4,24 +4,24 @@ from homeassistant.core import HomeAssistant
 
 _LOGGER = logging.getLogger(__name__)
 
-async def send_notification(hass: HomeAssistant, message, methods):
-    """Send notifications using specified methods."""
-    for method in methods:
-        if method == "persistent_notification":
-            await hass.services.async_call(
-                'persistent_notification',
-                'create',
-                {
-                    'title': 'Stark Energy Monitor Alert',
-                    'message': message,
-                }
-            )
-        elif method == "mobile_app":
-            await hass.services.async_call(
-                'notify',
-                'mobile_app',
-                {
-                    'message': message,
-                }
-            )
-        # Add other notification methods as needed
+# async def send_notification(hass: HomeAssistant, message, methods):
+#    """Send notifications using specified methods."""
+#    for method in methods:
+#        if method == "persistent_notification":
+#            await hass.services.async_call(
+#                'persistent_notification',
+#                'create',
+#                {
+#                    'title': 'Stark Energy Monitor Alert',
+#                    'message': message,
+#                }
+#            )
+#        elif method == "mobile_app":
+#            await hass.services.async_call(
+#                'notify',
+#                'mobile_app',
+#                {
+#                    'message': message,
+#                }
+#            )
+#        # Add other notification methods as needed
